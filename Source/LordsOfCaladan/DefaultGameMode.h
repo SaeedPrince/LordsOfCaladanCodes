9d5736800ec02bb6c214b0fdaf3690643aff2a3e
M Saied E Shoar <ssshoar@gmail.com>
2018-02-20 16:11:08 -0800

    Initialization

---
+++ Source/LordsOfCaladan/DefaultGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "DefaultGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LORDSOFCALADAN_API ADefaultGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
