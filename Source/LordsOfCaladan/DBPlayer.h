00dc90668cea505b0b72cf6e72cf4b3f3c03d5d9
ssshoar@gmail.com <ssshoar@gmail.com>
2018-04-15 14:40:05 +1200

    Added database and Player and Building classes.

---
+++ Source/LordsOfCaladan/DBPlayer.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LORDSOFCALADAN_API DBPlayer
+{
+public:
+	int32 Code;
+	FString Name;
+	FString Title;
+	DBPlayer();
+	~DBPlayer();
+};
