1d0f5d79b403051bf791a4c5ec8e8c07738e1d77
ssshoar@gmail.com <ssshoar@gmail.com>
2018-04-15 22:07:43 +1200

    Working on Game Structures...

---
+++ Source/LordsOfCaladan/DBFaction.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DBFaction.h"
+
+DBFaction::DBFaction()
+{
+}
+
+DBFaction::~DBFaction()
+{
+}
---
+++ Source/LordsOfCaladan/DBItem.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DBItem.h"
+
+DBItem::DBItem()
+{
+}
+
+DBItem::~DBItem()
+{
+}
